a3bb74046edb405ce72f642425a2178576e3577d
shamith01 <128206892+shamith01@users.noreply.github.com>
2023-04-06 21:00:02 -0700

    update 007

--- contents/indexc.js
+++ contents/indexc.js
@@ -10,46 +10,50 @@ An Indian Air Forces (IAF) Chinook helicopter was used to drop the RLV-TD from a
 “The RLV took off at 7:10 a.m. by a Chinook helicopter of the IAF as an underslung load and flew at a height of 4.5 km. Once the predetermined pillbox parameters were attained, based on the RLV’s Mission Management and Computer command, the RLV has released mid-air, at a down range of 4.6 km,” ISRO said.`;
 
 // home2
-const indexp2='';
-const indext2='';
-const indexd2='';
+const indexp2='https://www.hindustantimes.com/ht-img/img/2023/04/06/550x309/China-New-Year-0_1675277308583_1675277308583_1680789786371_1680789786371.jpg';
+const indext2=`China's Xi Jinping calls for Ukraine peace talks`;
+const indexd2=`Chinese leader Xi Jinping called Thursday for peace talks over Ukraine after French President Emmanuel Macron appealed to him to “bring Russia to its senses”.Chinese leader Xi Jinping called Thursday for peace talks over Ukraine after French President Emmanuel Macron appealed to him to “bring Russia to its senses,” but Xi gave no indication Beijing would use its leverage as Vladimir Putin's diplomatic partner to press for a settlement.`;
 
 // home3
-const indexp3='';
-const indext3='';
-const indexd3='';
+const indexp3='https://www.hindustantimes.com/ht-img/img/2023/04/06/550x309/CANADA-WEATHER--5_1680790935860_1680790935860_1680790962570_1680790962570.JPG';
+const indext3='Freezing rain in Canada leaves more than a million without power';
+const indexd3='There were 1,000 workers trying to restore power overnight and Thursday morning, electricity provider Hydro-Quebec said.More than a million people were without power in Quebec on Thursday morning after a day of freezing rain and strong winds hit southeastern Canada, toppling trees onto power lines.';
 
 // home4
-const indexp4='';
-const indext4='';
-const indexd4='';
+const indexp4='https://www.hindustantimes.com/ht-img/img/2023/04/06/550x309/sxsxs_1650611623617_1680792410086_1680792410086.jpg';
+const indext4=`Decide on AAP’s national party status by April 13: K'taka HC tells EC`;
+const indexd4='Justice M Nagaprasanna observed that it was the responsibility of the Commission to independently assess the representation and take a decision.The High Court of Karnataka on Thursday directed the Election Commission to consider the representation made by the Aam Aadmi Party for recognition as a national party as per law, and take a decision by April 13. Justice M Nagaprasanna, who heard the petition, observed that it was the responsibility of the Commission to independently assess the representation and take a decision.';
 
 // home5
-const indexp5='';
-const indext5='';
-const indexd5='';
+const indexp5='https://www.hindustantimes.com/ht-img/img/2023/04/06/550x309/Ex-PFI-chairman-E-Abubacker--who-was-arrested-in-S_1680784538479.jpg';
+const indext5='Delhi HC lets ex-PFI chairman Abubacker withdraw plea for bail on medical grounds';
+const indexd5=`Abubacker was arrested by the anti-terror probe agency during a massive crackdown on the banned organisation last year and is currently in judicial custody.
+NEW DELHI: The Delhi high court on Thursday permitted former Popular Front of India (PFI) chairman E Abubacker to withdraw his plea seeking release on account of ill health in a case registered under the Unlawful Activities Prevention Act.`;
 
 // home6
-const indexp6='';
-const indext6='';
-const indexd6='';
+const indexp6='https://www.hindustantimes.com/ht-img/img/2023/04/06/550x309/The-bench-said-Kanpur-Bar-Association-and-the-Lawy_1680793839587.jpg';
+const indext6='Lawyers’ strike: HC issues notices to office bearers of Kanpur bar association';
+const indexd6=`As an interim measure, the court directed that the lawyers shall resume their work forthwith and to purge the contempt and in case, any hindrance is created by any of the lawyer or office bearers, the same would be viewed seriously.
+PRAYAGRAJ A bench of seven judges of the Allahabad high court -- led by Chief Justice Pritinker Diwaker -- has issued notices to the office bearers of the Kanpur Bar Association for boycotting work in courts.`;
 
 // home7
-const indexp7='';
-const indext7='';
-const indexd7='';
+const indexp7='https://www.hindustantimes.com/ht-img/img/2023/04/06/550x309/Commuters-rush-to-catch-an-overcrowded-train-near-_1680793839586.jpg';
+const indext7='Special train between New Delhi-Katra to clear extra holiday rush';
+const indexd7=`Jammu railway station director Uchit Singhal said it will be a one trip special, which will be run to clear the extra rush of passengers
+JAMMU: The Railways have decided to run a special train between New Delhi and Katra town to clear the extra rush of passengers due to long weekend, said officials.`;
 
 // home8
-const indexp8='';
-const indext8='';
-const indexd8='';
+const indexp8='https://www.hindustantimes.com/ht-img/img/2023/04/06/550x309/_e1407ffc-9102-11ea-80d7-183d6ba34199_1680793683213_1680793683213.jpg';
+const indext8='KCET 2023 registration date extended till April 9, notification here';
+const indexd8=`KEA extended the registration deadline for the Karnataka Common Entrance Test (KCET) 2023 till April 9, 2023.
+The deadline for registering for the Karnataka Common Entrance Test (KCET) 2023 has been extended by the Karnataka Examination Authority (KEA) till April 9, 2023. Candidates can apply on the official website, kea.kar.nic.in, for the KCET 2023. Initially, the deadline for applications was April 5.`;
 
-export {indexp1,indext1,indexd1,};  
-//    ` indexp2,indext2,indexd2,
-//     indexp3,indext3,indexd3,
-//     indexp4,indext4,indexd4,
-//     indexp5,indext5,indexd5,
-//     indexp6,indext6,indexd6,
-//     indexp7,indext7,indexd7,
-//     indexp8,indext8,indexd8,`
+export {indexp1,indext1,indexd1,  
+    indexp2,indext2,indexd2,
+    indexp3,indext3,indexd3,
+    indexp4,indext4,indexd4,
+    indexp5,indext5,indexd5,
+    indexp6,indext6,indexd6,
+    indexp7,indext7,indexd7,
+    indexp8,indext8,indexd8,};
 
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import styles from '../styles/index.module.css'
 import Image from 'next/image'
 import Head from 'next/head'
 import {indexp1,indext1,indexd1,    
-  // `indexp2,indext2,indexd2,
-  // indexp3,indext3,indexd3,
-  // indexp4,indext4,indexd4,
-  // indexp5,indext5,indexd5,
-  // indexp6,indext6,indexd6,
-  // indexp7,indext7,indexd7,
-  // indexp8,indext8,indexd8,`
+  indexp2,indext2,indexd2,
+  indexp3,indext3,indexd3,
+  indexp4,indext4,indexd4,
+  indexp5,indext5,indexd5,
+  indexp6,indext6,indexd6,
+  indexp7,indext7,indexd7,
+  indexp8,indext8,indexd8,
 } from '../contents/indexc'
 
 function index() {
@@ -20,24 +20,24 @@ function index() {
       <meta name="description" content="get updated with the latest news" />
     </Head>
     <div className={styles.main}>
-      <div className={styles.image}>
-        <img className={styles.img} src={indexp1}  alt={'hlo'} /> 
-          
-      </div>
       <div className={styles.info}>
         <h2>{indext1}</h2>
+      </div>
+      <div className={styles.image}>
+        <img className={styles.img} src={indexp1}  alt={'img'} /> 
+          
       </div>
       <div className={styles.desc}>
         <p>{indexd1}</p>
       </div>
     </div>
-{/* 
+
     <div className={styles.main}>
-      <div className={styles.image}>
-        <img className={styles.img} src={indexp2} />
-      </div>
       <div className={styles.info}>
         <h2>{indext2}</h2>
+      </div>
+      <div className={styles.image}>
+        <img className={styles.img} src={indexp2} alt={'img'}/>
       </div>
       <div className={styles.desc}>
         <p>{indexd2}</p>
@@ -45,11 +45,11 @@ function index() {
     </div>
 
     <div className={styles.main}>
-      <div className={styles.image}>
-        <img className={styles.img} src={indexp3} />
-      </div>
       <div className={styles.info}>
         <h2>{indext3}</h2>
+      </div>
+      <div className={styles.image}>
+        <img className={styles.img} src={indexp3} alt={'img'}/>
       </div>
       <div className={styles.desc}>
         <p>{indexd3}</p>
@@ -57,11 +57,11 @@ function index() {
     </div>
 
     <div className={styles.main}>
-      <div className={styles.image}>
-        <img className={styles.img} src={indexp4} />
-      </div>
       <div className={styles.info}>
         <h2>{indext4}</h2>
+      </div>
+      <div className={styles.image}>
+        <img className={styles.img} src={indexp4} alt={'img'} />
       </div>
       <div className={styles.desc}>
         <p>{indexd4}</p>
@@ -69,11 +69,11 @@ function index() {
     </div>
 
     <div className={styles.main}>
-      <div className={styles.image}>
-        <img className={styles.img} src={indexp5} />
-      </div>
       <div className={styles.info}>
         <h2>{indext5}</h2>
+      </div>
+      <div className={styles.image}>
+        <img className={styles.img} src={indexp5} alt={'img'}/>
       </div>
       <div className={styles.desc}>
         <p>{indexd5}</p>
@@ -81,11 +81,11 @@ function index() {
     </div>
 
     <div className={styles.main}>
-      <div className={styles.image}>
-        <img className={styles.img} src={indexp6} />
-      </div>
       <div className={styles.info}>
         <h2>{indext6}</h2>
+      </div>
+      <div className={styles.image}>
+        <img className={styles.img} src={indexp6} alt={'img'}/>
       </div>
       <div className={styles.desc}>
         <p>{indexd6}</p>
@@ -93,11 +93,11 @@ function index() {
     </div>
 
     <div className={styles.main}>
-      <div className={styles.image}>
-        <img className={styles.img} src={indexp7} />
-      </div>
       <div className={styles.info}>
         <h2>{indext7}</h2>
+      </div>
+      <div className={styles.image}>
+        <img className={styles.img} src={indexp7} alt={'img'}/>
       </div>
       <div className={styles.desc}>
         <p>{indexd7}</p>
@@ -105,16 +105,16 @@ function index() {
     </div>
 
     <div className={styles.main}>
-      <div className={styles.image}>
-        <img className={styles.img} src={indexp8} />
-      </div>
       <div className={styles.info}>
         <h2>{indext8}</h2>
+      </div>
+      <div className={styles.image}>
+        <img className={styles.img} src={indexp8} alt={'img'}/>
       </div>
       <div className={styles.desc}>
         <p>{indexd8}</p>
       </div>
-    </div> */}
+    </div>
 
 
     

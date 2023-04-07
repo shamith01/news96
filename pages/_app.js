ded0676ad61cc442dec623be93a58fd4e6a750e8
shamith01 <128206892+shamith01@users.noreply.github.com>
2023-04-07 12:26:12 -0700

    sss

--- pages/_app.js
+++ pages/_app.js
@@ -10,8 +10,7 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" type="image/x-icon" href="/image/favicon.ico" />
         <meta name="description" content="get realtime news updates and stay updated wth job posting and latest sports,ipl news.stay updated with geoploitics" />
         <meta name="keywords" content="news96,newstoday,breaking news,news,india today,hlo india" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4700790597407744"
-     crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4700790597407744" crossorigin="anonymous"></script>
       </Head>
     
 
